47790228a1fb83ebbdf5dc970a9a507b131b459c
Angger Binuko Paksi <angger.binuko@gmail.com>
2023-03-29 09:59:17 +0700

    init project

---
+++ app/src/main/java/lab/binz/dailypractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package lab.binz.dailypractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
